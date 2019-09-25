3bfacc3121914040e23da0364d2c19898e9f7daa
Pavan Inguva <krishna.inguva15@imperial.ac.uk>
2019-09-25 03:25:07 +0100

    Update flow.sh for running on flow by copying relevant i/o files from MPI nohup run

--- flow.sh
+++ flow.sh
@@ -5,4 +5,5 @@ singularity exec ./ocellaris_2019.0.2.sif mpirun -np 8 python3 binary.py 2>outpu
 foldername=$(date +%Y%m%d_%H%M%S);
 mkdir "$foldername";
 cp params.yml ./"$foldername";
-mv output* ./"$foldername"
+mv output* ./"$foldername";
+mv nohup* ./"$foldername"
