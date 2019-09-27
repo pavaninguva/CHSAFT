75da8fa4fa496820748e063e7bfd4f4976e3ad1b
Pavan Inguva <krishna.inguva15@imperial.ac.uk>
2019-09-27 09:30:06 +0100

    TEST: Fiddle around with parameters, still unstable.

--- flow.sh
+++ flow.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-singularity exec ./ocellaris_2019.0.2.sif mpirun -np 8 python3 binary.py 2>outputerr.txt;
+singularity exec ./ocellaris_2019.0.2.sif mpirun -np 12 python3 binary.py 2>outputerr.txt;
 
 foldername=$(date +%Y%m%d_%H%M%S);
 mkdir "$foldername";
