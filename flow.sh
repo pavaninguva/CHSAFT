aadca2a819df7fba8cb16eca9f9d750dfaf43602
pavaninguva <krishna.inguva15@imperial.ac.uk>
2020-02-19 06:43:59 +0000

    FIX: Modify flow.sh script for running on flow

--- flow.sh
+++ flow.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
-conda activate fipy34;
 
-mpirun -np 8 python finitevolume.py;
+# conda activate fipy34;
+
+# mpirun -np 8 python finitevolume.py;
+
+# foldername=$(date +%Y%m%d_%H%M%S);
+# mkdir "$foldername";
+# cp params.yml ./"$foldername";
+# mv *.vtk ./"$foldername";
+# mv nohup* ./"$foldername"
+
+singularity exec ./ocellaris_2019.0.2.sif mpirun -np 12 python3 binary.py 2>outputerr.txt;
 
 foldername=$(date +%Y%m%d_%H%M%S);
 mkdir "$foldername";
 cp params.yml ./"$foldername";
-mv *.vtk ./"$foldername";
+mv output* ./"$foldername";
 mv nohup* ./"$foldername"
